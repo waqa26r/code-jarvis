bb5ab39c7ad8e0efce9b5917d12ad8ff1bfc5e5b
waqa26r <115084710+waqa26r@users.noreply.github.com>
2023-08-26 00:10:08 +0530

    Update jrvs.py

--- jrvs.py
+++ jrvs.py
@@ -26,7 +26,7 @@ def wishMe():
 
     speak("Its my pleasure that I am following   you sir!!! ")
    
-    speak("Muhammad","Abu-Bakar","Omar Farooq","Ali-Ibne Haider","Hasan","Husain","..")
+    speak("Muhammad","Abu-Bakar"," Omar Ibne-Khattab","Ali-Ibne Haider","Hasan","Husain","..")
 
 
 def takecommand():
